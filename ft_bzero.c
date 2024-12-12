b4ce660ed68e27a2979490ae8f2ec15b2b648c99
Mhuescarb <mhuescar@student.42malaga.com>
2024-12-12 10:02:52 +0100

    bzero and starting memcpy

---
+++ ft_bzero.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void ft_bzero (void *s, size_t n)
+{
+int i;
+i = 0;
+char *ptr;
+ptr = (char *)s;
+
+    while (i < n)
+    {
+        ptr[i] = 0;
+        i++;
+    }
+}
+int main() 
+{
+    char buffer[10] = "HelloWorld";
+    printf("Antes de ft_bzero: %s\n", buffer);
+    
+    ft_bzero(buffer, 5);
+    
+    printf("Después de ft_bzero: %s\n", buffer);
+    
+    return 0;
+}
