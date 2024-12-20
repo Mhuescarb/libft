87473055632d4ad594dfc9d831949741c72d3670
Mhuescarb <mhuescar@student.42malaga.com>
2024-12-20 21:38:54 +0100

    fixing tests

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: mhuescar <mhuescar@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 10:38:26 by mhuescar          #+#    #+#             */
-/*   Updated: 2024/12/20 20:16:27 by mhuescar         ###   ########.fr       */
+/*   Updated: 2024/12/20 21:38:03 by mhuescar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlen(const char *str)
+/*int	ft_strlen(const char *str)
 {
 	int	i;
 
@@ -22,7 +22,7 @@ int	ft_strlen(const char *str)
 		i++;
 	}
 	return (i);
-}
+}*/
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
@@ -31,25 +31,30 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	i = 0;
 	if (size == 0)
 		return (ft_strlen (src));
-	while (src [i] != '\0' && i < size -1)
+	while (src [i] != '\0' && i < (size -1))
 	{
 		dst[i] = src [i];
 		i++;
-		dst [i] = '\0';
 	}
+	dst [i] = '\0';
 	return (ft_strlen(src));
 }
 
-/* #include <stdio.h>
+/*#include <stdio.h>
+#include <string.h>
 int main()
 {
-    char src[] = "Hola, mundo!";
-    char dst[10];
+    char src[] = "coucou";
+    char dst[10]; memset(dst, 'A', 10);
 
-    size_t copied = ft_strlcpy(dst, src, sizeof(dst));
+    size_t copied = ft_strlcpy(dst, src, 1);
 
     printf("Cadena copiada: %s\n", dst);
     printf("Longitud de la cadena original: %zu\n", copied);
+	printf("Test: %lu\n", strlen(src));
+	printf("Test: %c\n", dst[0]); // 0
+	printf("Test: %c\n", dst[1]); // A
+	//printf("Test: %d", strlen(src) && dst[0] == 0 && dst[1] == 'A');
 
     return 0;
 }*/
